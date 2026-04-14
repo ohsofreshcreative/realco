6625054416e332b4330e2172ea3bcfa09f70ee16
ohsofreshcreative <132752926+ohsofreshcreative@users.noreply.github.com>
2026-04-14 14:05:00 +0200

    Automatyczny deploy

--- resources/js/blocks/hero.js
+++ resources/js/blocks/hero.js
@@ -1,17 +1,17 @@
 import Swiper from 'swiper';
-import { Pagination } from 'swiper/modules';
+import { Pagination, Autoplay } from 'swiper/modules'; // <-- Dodaj Autoplay
 import 'swiper/css';
 import 'swiper/css/pagination';
 
 document.querySelectorAll('.hero-slider').forEach((slider) => {
   new Swiper(slider, {
-    modules: [Pagination],
+    modules: [Pagination, Autoplay], // <-- Dodaj Autoplay do modułów
     slidesPerView: 1,
     spaceBetween: 0,
     loop: true,
-	autoplay: {
-	  delay: 5000,
-	},
+    autoplay: {
+      delay: 5000,
+    },
     pagination: {
       el: slider.querySelector('.swiper-pagination'),
       clickable: true,
